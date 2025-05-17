4e1324e3745617a6a3457db58a32e8c6f19dde55
Pınar Nida Tunca <tpinarnida10@gmail.com>
2025-05-18 02:04:08 +0300

    renkler.dart

---
+++ renkler.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+var anarenk=const Color(0xFFCB2020);
+var yazirenk1=const Color(0xFFFFFFFF);
+var yazirenk2=const Color(0xFF636363);
+var yazirenk3=const Color(0xFF0D0C0C);
+var appbar=const Color(0xFFFFB74D);
+var ab=const Color(0xFFFFCC80);
+var ab2=const Color(0xFFFFA726);
